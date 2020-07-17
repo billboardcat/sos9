7b39e68430b8cd29ad534c84ef9453d196c286c7
Jimmy Sung <billboardcat@gmail.com>
2020-07-17 15:26:57 -0400

    Added more teacher questions to the sidebar.

--- app.py
+++ app.py
@@ -10,20 +10,49 @@ default_pwd = "123"
 
 def main():
     st.subheader("Hello, " + session_state.username + "!")
-    st.sidebar.header("Step 1 - Get started by uploading your student data:")
+    st.info("Get started by answering some questions about your course in the sidebar")
+    st.sidebar.header("Step 1 - Upload your student data:")
     file = st.sidebar.file_uploader("Upload a CSV file, max 200 MB", type='csv')
     if file is not None:
         my_DF = pd.read_csv(file)
         st.write(my_DF)
+
     st.sidebar.header("Step 2 - Tell us more about your course's structure:")
-    course_duration = st.sidebar.number_input(label="Course duration (in weeks)", min_value=1)
+
+    # Yes/No questions, some will prompt for more information
     graded_attendance = st.sidebar.checkbox(label='Graded attendance')
     lecture_online = st.sidebar.checkbox(label='Lecture materials are uploaded')
     supp_readings = st.sidebar.checkbox(label='Supplemental readings available')
     practice_exams = st.sidebar.checkbox(label='Practice exams available')
-    lecture_quizzes = st.sidebar.checkbox(label='In-class quizzes')
-    lecture_clickers = st.sidebar.checkbox(label='In-class questions (i.e. clicker questions)')
     course_calendar = st.sidebar.checkbox(label='Course calendar available')
+
+    lecture_quizzes = st.sidebar.checkbox(label='In-class quizzes')
+    quiz_count = 0 if not lecture_quizzes else st.sidebar.number_input(label="Quiz count", min_value=1)
+
+    lecture_clickers = st.sidebar.checkbox(label='In-class questions (i.e. clicker questions)')
+    clicker_freq = None if not lecture_clickers else st.sidebar.selectbox(label='Clicker frequency', options=('Rarely', 'Sometimes', 'Often'))
+
+    homeworks_given = st.sidebar.checkbox(label='Homework given')
+    homework_count = 0 if not homeworks_given else st.sidebar.number_input(label='Homework count', min_value=1)
+    homework_length = 0 if not homeworks_given else st.sidebar.number_input(label='Avg homework length (hours)', min_value=0.0, value=0.0, step=0.5)
+
+    projects_given = st.sidebar.checkbox(label='Projects given')
+    project_count = 0 if not projects_given else st.sidebar.number_input(label='Project count', min_value=1)
+
+    office_hours_offered = st.sidebar.checkbox(label='Office hours offered')
+    avg_office_hrs_student_count = 0 if not office_hours_offered else st.sidebar.number_input(label='Avg # of students attending office hours, per week')
+
+    tas_available = st.sidebar.checkbox(label='TAs available')
+    ta_count = 0 if not tas_available else st.sidebar.number_input(label='Number of TAs available', min_value=1)
+
+    avg_lecture_student_count = st.sidebar.number_input(label='Avg # of students attending lectures, per lecture')
+
+    lecture_duration = st.sidebar.number_input(label="Lecture duration (in hours)", min_value=1.0, step=0.5)
+    course_duration = st.sidebar.number_input(label="Course duration (in weeks)", min_value=1)
+    term_duration = st.sidebar.number_input(label="School term duration (in weeks)", min_value=1)
+    if term_duration < course_duration:
+        st.error("Error: School term duration cannot be smaller than course duration")
+
 
 
 if session_state.password != default_pwd:
