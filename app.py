214276c4457412af20ec1bdfc5e2b403f518d0fd
Jimmy Sung <billboardcat@gmail.com>
2020-07-20 23:05:14 -0400

    4 questions of 5 done.

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import pandas as pd
 from SessionState import get
 import sidebar
 from PIL import Image
+import matplotlib.pyplot as plt
+import numpy as np
 
 session_state = get(username='', password='', is_active=False, step1_done=False)
 default_pwd = "SOSdemopassword123"
@@ -13,6 +15,7 @@ def main():
     # Generate welcome message
     st.subheader("Hello, " + session_state.username + "! :smile:")
     start_placeholder = st.empty()
+
     if not session_state.step1_done:
         start_placeholder.info("Get started by uploading your course data in the sidebar")
 
@@ -36,13 +39,75 @@ def main():
     file = st.sidebar.file_uploader("Upload a CSV file, max 200 MB", type='csv')
     if file is not None:
         session_state.step1_done = True
-        my_DF = pd.read_csv(file)
-        st.write(my_DF)
+        start_placeholder.empty()
+        st.markdown("---")
+        st.subheader("Here's the raw data. Make sure everything's looking right before continuing!")
+        myDF = pd.read_csv(file)
+        st.write(myDF.head(5))
 
-        final_avgs_commMethod = my_DF.groupby(by='commMethod')['finalGrade'].mean()
-        corr_matrix = my_DF.corr()
+        # Q1: methods of communication v. final grades
+        st.markdown("---")
+        plt.style.use('ggplot')  # Personal style preference
+        q1 = myDF.groupby(by='commMethod')['finalGrade'].mean()  # Grouping and averaging grades
+        plt.bar(q1.index.values, q1.values, color='blue')  # Bar Plot
+        plt.ylabel("Average Grade")
+        plt.xlabel("Professor's Communication")
+        plt.title("Comparing methods of Professor Communication via final grades", fontsize=12)
+        st.pyplot()
 
-        final_avgs_commMethod.plot.bar(x='Communication Method', y='Final Average', rot=0)
+        # Q2: graded attendance v. student interaction
+        st.markdown("---")
+        myDF['gradeAttendance'] = myDF['gradeAttendance'].replace({1: 'Yes', 0: 'No'})
+        q2 = myDF.groupby(by='gradeAttendance')['attendanceCount'].mean()
+        plt.bar(q2.index.values, q2.values, color='red')  # Bar Plot
+        plt.ylabel("Lecture Attendance Count")
+        plt.xlabel("Does the Professor Grade Attendance?")
+        plt.title("Does Graded Attendance Increase Student Interaction?")
+        st.pyplot()
+
+        # Q3:
+        st.markdown("---")
+        q3DF = myDF[['attendanceCount', 'finalGrade']]
+        corr = float(q3DF.corr().iloc[0, 1])
+
+        st.header("Attendance and Assignment Grades")
+        st.subheader("Correlation coefficient: {:.4f}".format(corr))
+        if corr > 0.9:
+            st.subheader('There is likely a very high positive correlation between attendance and assignment grades')
+        if 0.9 > corr >= 0.7:
+            st.subheader('There is likely a high positive correlation between attendance and assignment grades')
+        if 0.7 > corr >= 0.5:
+            st.subheader('There is likely moderate positive correlation between attendance and assignment grades')
+        if 0.5 > corr >= 0.3:
+            st.subheader('There is likely slight positive correlation between attendance and assignment grades')
+        if 0.3 > corr >= 0.0:
+            st.subheader('There is likely no correlation between attendance and assignment grades')
+        if corr < 0.0:
+            st.subheader('There is likely a negative correlation between attendance and assignment grades')
+
+        # Q4
+        st.markdown("---")
+        st.header("Attendance and Lecture Engagement")
+        dist = np.random.normal(9, 3, size=len(myDF['finalGrade']))
+        dist = [int(i) for i in dist]
+        myDF['questions'] = dist
+
+        q4DF = myDF[['questions', 'finalGrade']]
+        corr = float(q4DF.corr().iloc[0, 1])
+        st.subheader("Correlation Coefficient: {:.4f}".format(corr))
+
+        if corr > 0.9:
+            st.subheader('There is likely a very high positive correlation between attendance and lecture engagement')
+        if 0.9 > corr >= 0.7:
+            st.subheader('There is likely a high positive correlation between attendance and lecture engagement')
+        if 0.7 > corr >= 0.5:
+            st.subheader('There is likely moderate positive correlation between attendance and lecture engagement')
+        if 0.5 > corr >= 0.3:
+            st.subheader('There is likely slight positive correlation between attendance and lecture engagement')
+        if 0.3 > corr >= 0.0:
+            st.subheader('There is likely no correlation between attendance and lecture engagement')
+        if corr < 0.0:
+            st.subheader('There is likely a negative correlation between attendance and lecture engagement')
 
 
 if session_state.password != default_pwd:
@@ -65,7 +130,7 @@ if session_state.password != default_pwd:
         pwd = pwd_placeholder.text_input("Password:", value=default_pwd, type="password")
         is_pressed = btn_placeholder.button("Login")
         session_state.username = usr
-        print(session_state.is_active)
+
         if len(usr) == 0:   # If the user does not input a username, default to "Guest"
             session_state.username = "Guest"
         if is_pressed:                      # When login button is pressed
